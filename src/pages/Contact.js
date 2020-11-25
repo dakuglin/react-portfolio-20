9612fa6f49ea09ae61a7a6142e36e3b0867bba4d
Dana Kuglin <dakuglin@gmail.com>
2020-11-25 13:38:10 -0700

    added contact page form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,28 @@
 import React from "react";
+import "../styles/contact.css";
 
 function Contact() {
     return(
         <>
-        <p>Contact Page</p>
+        <div className="container-fluid">
+            <h3>Please contact me about your web development needs</h3>
+            <form>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                </div>
+                <div class="form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
         </>
     )
 }
