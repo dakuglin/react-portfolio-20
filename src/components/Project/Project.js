c9a4ddb601062df25e568db518ebfe8e00d3c6a2
Dana Kuglin <dakuglin@gmail.com>
2020-11-28 16:31:50 -0700

    added styling

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -6,15 +6,15 @@ function Project(props) {
         <>
     
         <div class="row">
-        <div class="card  col-xs-12 col-md-4">
-            {/* <img class="card-img-top" src="asset/images/dog friendly denver.JPG" alt="Dog Friendly Denver Homepage"/>  */}
-            <div class="card-body">
-                <h5 class="card-title">{props.name}</h5>
-                <p class="card-text">{props.description}</p>
-                <a href={props.deploy} target="_blank" class="btn btn-primary">deploy</a> 
-                <a href={props.github} target="_blank" class="btn btn-primary">github</a> 
+            <div class="card  col-xs-12 col-md-4">
+                {/* <img class="card-img-top" src="asset/images/dog friendly denver.JPG" alt="Dog Friendly Denver Homepage"/>  */}
+                <div class="card-body">
+                    <h5 class="card-title">{props.name}</h5>
+                    <p class="card-text">{props.description}</p>
+                    <a href={props.github} target="_blank" class="btn btn-primary">github</a> 
+                    <a href={props.deploy} target="_blank" class="btn btn-primary">deploy</a>  
+                </div>
             </div>
-        </div>
             
         </div>
         </>
